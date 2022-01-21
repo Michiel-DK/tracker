b0bddb5def4362cff968ad1b299e798172d24bdc
Michiel <michieldekoninck2@gmail.com>
2022-01-21 13:40:22 +0000

    posstgres

---
+++ tracker/postgres.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import psycopg2
+#from config import config
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+
+dotenv_path = Path('../.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = {
+            'host':os.getenv('postgres_host'),
+            'database':os.getenv('postgres_database'),
+            'user':os.getenv('postgres_user'),
+            'password':os.getenv('postgres_password')
+        }
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
