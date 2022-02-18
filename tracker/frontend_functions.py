1d4510b451e7a727927bbace3ead17d3c052b45f
Michiel De Koninck <michieldekoninck2@gmail.com>
2022-02-18 15:21:33 +0000

    update queries from docker container

--- tracker/frontend_functions.py
+++ tracker/frontend_functions.py
@@ -1,64 +1,77 @@
-import psycopg2
 from tracker.postgres import connect, config
 import pandas as pd
 import numpy as np
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import dotenv
 
-params = config(filename='/Users/michieldekoninck/code/Michiel-DK/tracker/database.ini', section='postgresql')
+#params = config(filename='/Users/michieldekoninck/code/Michiel-DK/tracker/database.ini', section='postgresql')
 
-conn = psycopg2.connect(**params)
+#conn = psycopg2.connect(**params)
+
+from dotenv import dotenv_values
+
+database_env = dotenv_values("database.env")
+
+engine = create_engine(f"postgresql://{database_env['POSTGRES_USER']}:{database_env['POSTGRES_PASSWORD']}@localhost:{database_env['POSTGRES_PORT']}/{database_env['POSTGRES_DB']}")
+Session = sessionmaker(bind=engine)
 
 def get_quarterly_moat(ticker):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from quarterly_moat WHERE ticker = '{ticker}'""")
-    selected_moat = cur.fetchall()
-    cur.close();
-    colnames = [desc[0] for desc in cur.description]
+    #cur = conn.cursor()
+    #cur.execute(f"""SELECT * from quarterly_moat WHERE ticker = '{ticker}'""")
+    #selected_moat = cur.fetchall()
+    #cur.close();
+    #colnames = [desc[0] for desc in cur.description]
+    session = Session()
+    query = text(f"SELECT * from quarterly_moat WHERE ticker = '{ticker}'")
+    selected_moat = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     selected_moat_df = pd.DataFrame(selected_moat, columns=colnames).drop(columns='index')
     selected_moat_df.index = selected_moat_df['year']
     selected_moat_df.drop(columns = ['year', 'ticker', 'key'], inplace=True)
     return selected_moat_df
 
 def get_quarterly_health(ticker):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from quarterly_health WHERE ticker = '{ticker}'""")
-    selected_moat = cur.fetchall()
-    cur.close();
-    colnames = [desc[0] for desc in cur.description]
-    selected_health_df = pd.DataFrame(selected_moat, columns=colnames).drop(columns='index')
+    session = Session()
+    query = text(f"SELECT * from quarterly_health WHERE ticker = '{ticker}'")
+    selected_health = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
+    selected_health_df = pd.DataFrame(selected_health, columns=colnames).drop(columns='index')
     selected_health_df.index = selected_health_df['year']
     selected_health_df.drop(columns = ['year', 'ticker', 'key'], inplace=True)
     return selected_health_df
 
 
 def get_yearly_moat(ticker):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from yearly_moat WHERE ticker = '{ticker}'""")
-    selected_moat = cur.fetchall()
-    cur.close();
-    colnames = [desc[0] for desc in cur.description]
+    session = Session()
+    query = text(f"SELECT * from yearly_moat WHERE ticker = '{ticker}'")
+    selected_moat = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     selected_moat_df = pd.DataFrame(selected_moat, columns=colnames).drop(columns='index')
     selected_moat_df.index = selected_moat_df['year']
     selected_moat_df.drop(columns = ['year', 'ticker', 'key'], inplace=True)
     return selected_moat_df
 
 def get_yearly_health(ticker):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from yearly_health WHERE ticker = '{ticker}'""")
-    selected_moat = cur.fetchall()
-    cur.close();
-    colnames = [desc[0] for desc in cur.description]
-    selected_health_df = pd.DataFrame(selected_moat, columns=colnames).drop(columns='index')
+    session = Session()
+    query = text(f"SELECT * from yearly_health WHERE ticker = '{ticker}'")
+    selected_health = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
+    selected_health_df = pd.DataFrame(selected_health, columns=colnames).drop(columns='index')
     selected_health_df.index = selected_health_df['year']
     selected_health_df.drop(columns = ['year', 'ticker', 'key'], inplace=True)
     return selected_health_df
 
 def get_weekly(ticker):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from weekly_info WHERE ticker = '{ticker}'""")
-    #cur.execute(f"""SELECT * from weekly_info WHERE (weekly_info.ticker = '{ticker}' AND (weekly_info.date = (SELECT MAX(weekly_info.date) FROM weekly_info))) GROUP BY weekly_info.date""")
-    weekly = cur.fetchall()
-    cur.close();
-    colnames = [desc[0] for desc in cur.description]
+    session = Session()
+    query = text(f"SELECT * from weekly_info WHERE ticker = '{ticker}'")
+    weekly = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     weekly_info = pd.DataFrame(weekly, columns=colnames).drop(columns='index')
     weekly_info.index = weekly_info['date']
     company_info = weekly_info[['ticker','logo_url','sector', 'marketcap', 'market', 'longname', 'longbusinesssummary', 'industry', 'enterprisevalue']]
@@ -70,30 +83,30 @@ def get_weekly(ticker):
 
 def get_scanner_q():
     # get moat
-    cur = conn.cursor()
-    cur.execute('''SELECT * from quarterly_moat''')
-    colnames = [desc[0] for desc in cur.description]
-    moat = cur.fetchall()
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from quarterly_moat")
+    moat = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     q_moat = pd.DataFrame(moat, columns=colnames).drop(columns='index')
     #get health
-    cur = conn.cursor()
-    cur.execute('''SELECT * from quarterly_health''')
-    colnames = [desc[0] for desc in cur.description]
-    health = cur.fetchall()
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from quarterly_health")
+    health = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     q_health = pd.DataFrame(health, columns=colnames).drop(columns='index')
     #get weekly
-    cur = conn.cursor()
-    cur.execute('''SELECT * from weekly_info''')
-    weekly = cur.fetchall()
-    colnames = [desc[0] for desc in cur.description]
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from weekly_info")
+    weekly = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     weekly = pd.DataFrame(weekly, columns=colnames).drop(columns='index')
     weekly = weekly[['ticker','totalcashpershare', 'enterprisetorevenue', 'enterprisetoebitda', 'beta','longname','sector', 'industry', 'market', 'country']]
     #combine
     ls = []
-    for i in q_moat[q_moat['year']=='2021Q4']['ticker']:
+    for i in q_moat[q_moat['year']=='2021Q3']['ticker']:
         ls.append([i, np.round(np.mean(q_moat[q_moat['ticker']==i]['moatpercentage']),2) , np.round(np.mean(q_health[q_health['ticker']==i]['percentage']),2), \
             np.mean([np.round(np.mean(q_moat[q_moat['ticker']==i]['moatpercentage']),2),np.round(np.mean(q_health[q_health['ticker']==i]['percentage']),2)])])
     check_2021_q = pd.DataFrame(ls, columns=['ticker', 'moat', 'health', 'avg'])
@@ -103,25 +116,25 @@ def get_scanner_q():
 
 def get_scanner_y():
     # get moat
-    cur = conn.cursor()
-    cur.execute('''SELECT * from yearly_moat''')
-    colnames = [desc[0] for desc in cur.description]
-    moat = cur.fetchall()
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from yearly_moat")
+    moat = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     q_moat = pd.DataFrame(moat, columns=colnames).drop(columns='index')
     #get health
-    cur = conn.cursor()
-    cur.execute('''SELECT * from yearly_health''')
-    colnames = [desc[0] for desc in cur.description]
-    health = cur.fetchall()
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from yearly_health")
+    health = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     q_health = pd.DataFrame(health, columns=colnames).drop(columns='index')
     #get weekly
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * from weekly_info""")
-    weekly = cur.fetchall()
-    colnames = [desc[0] for desc in cur.description]
-    cur.close();
+    session = Session()
+    query = text(f"SELECT * from weekly_info")
+    weekly = session.execute(query).fetchall()
+    colnames = session.execute(query).keys()
+    session.close()
     weekly = pd.DataFrame(weekly, columns=colnames).drop(columns='index')
     weekly = weekly[['ticker','totalcashpershare', 'enterprisetorevenue', 'enterprisetoebitda', 'beta','longname','sector', 'industry', 'market', 'country']]
     #combine
