d56b930e867daa3bd8179511dd00b288d7317c74
Michiel De Koninck <michieldekoninck2@gmail.com>
2022-03-29 21:13:09 +0100

    update weekly changes

--- tracker/update_weekly.py
+++ tracker/update_weekly.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 import os, requests, urllib3, time
 from tracker.utils import copy_from_stringio
 from dotenv import load_dotenv
-
+import pandas as pd
 
 load_dotenv()
 
@@ -25,9 +25,13 @@ def update_weekly(ticker, engine):
         print(f"p - {ticker} - {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
             
 if __name__ == '__main__':
-        tickers = get_all_tickers()
+        #tickers = get_all_tickers()
         start = time.localtime(time.time())
         print(f"Started run at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+        full = list(pd.read_csv('../tracker/data/ticks.csv', sep=';')['ticks'])
+        tickers = full[:2]
+        del full[:2]
+        pd.DataFrame.to_csv(pd.DataFrame({'ticks': full}), '../tracker/data/ticks.csv', sep=';')
         for ticker in tickers:
                 print(ticker)
                 try:
