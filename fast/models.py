f670f4419d71ffc3270655da9f2649cfb5f8b9de
Michiel De Koninck <michieldekoninck2@gmail.com>
2022-12-08 11:44:06 +0000

    models updated to api call

--- fast/models.py
+++ fast/models.py
@@ -16,35 +16,105 @@ class Prices(Base):
 
 class Weekly(Base):
     __tablename__ = "weekly_info"
-
-
+    #id columns
     index = Column(Integer, primary_key=False, index=True)
     ticker = Column(String, primary_key=False, index=False)
     key = Column(String, unique=True, primary_key=True, index=False)
     date = Column(Date, primary_key=False, index=False)
-    grossmargins = Column(Float, primary_key=False, index=False)
+    
+    #general columns
+    bookvalue = Column(Float, primary_key=False, index=False)
+    country = Column(String, primary_key=False, index=False)
+    currency = Column(String, primary_key=False, index=False)
+    currentprice: Column(Float, primary_key=False, index=False)
+    enterprisevalue = Column(Integer, primary_key=False, index=True)
+    exchange = Column(String, primary_key=False, index=False)
+    fulltimeemployees = Column(Integer, primary_key=False, index=True)
+    sector = Column(String, primary_key=False, index=False)
+    industry = Column(String, primary_key=False, index=False)
+    isesgpopulated = Column(Boolean, primary_key=False, index=False)
+    longbusinesssummary = Column(String, primary_key=False, index=False)
+    longname = Column(String, primary_key=False, index=False)
+    market = Column(String, primary_key=False, index=False)
+    marketcap = Column(Integer, primary_key=False, index=False)
+    beta : Column(Float, primary_key=False, index=False)
+    beta3year = Column(String, primary_key=False, index=False)
+    
+    #analyst columns
+    recommendationmean = Column(Float, primary_key=False, index=False)
+    recommendationkey = Column(String, primary_key=False, index=False)
+    morningstaroverallrating = Column(String, primary_key=False, index=False)
+    morningstarriskrating = Column(String, primary_key=False, index=False)
+    numberofanalystopinions = Column(Integer, primary_key=False, index=True)
+    sharesshort = Column(Integer, primary_key=False, index=True)
+    shortratio = Column(Float, primary_key=False, index=False)
+    targetlowprice = Column(Float, primary_key=False, index=False)
+    targethighprice = Column(Float, primary_key=False, index=False)
+    targetmedianprice = Column(Float, primary_key=False, index=False)
+    targetmeanprice = Column(Float, primary_key=False, index=False)
+    
+    #value columns
     currentratio = Column(Float, primary_key=False, index=False)
     debttoequity = Column(Float, primary_key=False, index=False)
-    dividendrate = Column(Float, primary_key=False, index=False)
-    dividendyield = Column(Float, primary_key=False, index=False)
-    earningsgrowth = Column(Float, primary_key=False, index=False)
-    earningsquarterlygrowth = Column(Float, primary_key=False, index=False)
     enterprisetoebitda = Column(Float, primary_key=False, index=False)
     enterprisetorevenue = Column(Float, primary_key=False, index=False)
+    forwardpe = Column(Float, primary_key=False, index=False)
+    trailingpe = Column(Float, primary_key=False, index=False)
+    pricetosalestrailing12months = Column(Float, primary_key=False, index=False)
+    pricetobook = Column(Float, primary_key=False, index=False)
+    quickratio = Column(Float, primary_key=False, index=False)
+    
+    #dividend columns
+    dividendrate = Column(Float, primary_key=False, index=False)
+    dividendyield = Column(Float, primary_key=False, index=False)
+    fiveyearavgdividendyield = Column(Float, primary_key=False, index=False)
+    exdividenddate = Column(Integer, primary_key=False, index=True)
+    payoutratio = Column(Float, primary_key=False, index=False)
+    
+    #growth columns
+    earningsgrowth = Column(Float, primary_key=False, index=False)
+    earningsquarterlygrowth = Column(Float, primary_key=False, index=False)
+    revenuegrowth = Column(Float, primary_key=False, index=False)
+    revenuequarterlygrowth = Column(String, primary_key=False, index=False)
+    freecashflow = Column(Integer, primary_key=False, index=True)
+    
+    #profitability columns
+    ebitdamargins = Column(Float, primary_key=False, index=False)
+    grossmargins = Column(Float, primary_key=False, index=False)
+    returnonassets = Column(Float, primary_key=False, index=False)
+    returnonequity = Column(Float, primary_key=False, index=False)
+    profitmargins = Column(Float, primary_key=False, index=False)
+    
+    #price columns
+    currentprice = Column(Float, primary_key=False, index=False)
     fiftydayaverage = Column(Float, primary_key=False, index=False)
     fiftytwoweeklow = Column(Float, primary_key=False, index=False)
     fiftytwoweekhigh = Column(Float, primary_key=False, index=False)
-    currentprice = Column(Float, primary_key=False, index=False)
-    fiveyearavgdividendyield = Column(Float, primary_key=False, index=False)
-    freecashflow = Column(Integer, primary_key=False, index=True)
-    marketcap = Column(Integer, primary_key=False, index=True)
-    pricetobook = Column(Float, primary_key=False, index=False)
-    quickratio = Column(Float, primary_key=False, index=False)
-    returnonassets = Column(Float, primary_key=False, index=False)
-    returnonequity = Column(Float, primary_key=False, index=False)
-    revenuequarterlygrowth = Column(String, primary_key=False, index=False)
-    sector = Column(String, primary_key=False, index=False)
-    industry = Column(String, primary_key=False, index=False)
+    twohundreddayaverage = Column(Float, primary_key=False, index=False)
+    
+    #momentum columns
+    fiveyearaveragereturn = Column(String, primary_key=False, index=False)
+    threeyearaveragereturn = Column(String, primary_key=False, index=False)
+    
+    #earnings columns
+    forwardeps = Column(Float, primary_key=False, index=False)
+    trailingeps = Column(Float, primary_key=False, index=False)
+    
+    #ownership columns
+    heldpercentinsiders = Column(Float, primary_key=False, index=False)
+    heldpercentinstitutions = Column(Float, primary_key=False, index=False)
+    impliedsharesoutstanding = Column(String, primary_key=False, index=False)
+    floatshares = Column(Integer, primary_key=False, index=True)
+    sharesoutstanding = Column(Integer, primary_key=False, index=True)
+    
+    #balance columms
+    totalassets = Column(String, primary_key=False, index=True)
+    totalcash = Column(Integer, primary_key=False, index=True)
+    totalcashpershare = Column(Float, primary_key=False, index=False)
+    totaldebt = Column(Integer, primary_key=False, index=True)
+    totalrevenue = Column(Integer, primary_key=False, index=True)
+    grossprofits = Column(Integer, primary_key=False, index=True)
+    operatingcashflow = Column(Integer, primary_key=False, index=True)
 
     #items = relationship("Item", back_populates="owner")
     
--- fast/schemas.py
+++ fast/schemas.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 import datetime
 from pydantic import BaseModel
 
@@ -30,35 +30,144 @@ class PricesBase(BaseModel):
 class PricesCreate(PricesBase):
     pass
 
-#to have common attributes when reading or creating
-class WeeklyBase(BaseModel):
+class Weekly_Id(BaseModel):
+    """Id subclass for weekly api call"""
     ticker: str
     key: str
     date: datetime.date
-    grossmargin: Optional[float] = None
+    
+class Weekly_General(BaseModel):
+    '''General subclass for weekly api call'''
+    bookvalue: Optional[float] = None
+    country: Optional[str] = None
+    currency: Optional[str] = None
+    currentprice: Optional[float] = None
+    enterprisevalue : Optional[int] = None
+    exchange : Optional[str] = None
+    fulltimeemployees : Optional[int] = None
+    sector: Optional[str] = None
+    industry: Optional[str] = None
+    isesgpopulated: Optional[bool] = None
+    longbusinesssumary: Optional[str] = None
+    longname: Optional[str] = None
+    market : Optional[str] = None
+    marketcap: Optional[int] = None
+    beta : Optional[float] = None
+    beta3year : Optional[str] = None
+    
+class Weekly_Analysts(BaseModel):
+    '''Analyst subclass for weekly api call'''
+    recommendationmean: Optional[float] = None
+    recommendationkey: Optional[str] = None
+    morningstaroverallrating: Optional[str] = None
+    morningstarriskrating: Optional[str] = None
+    numberofanalystopinions: Optional[int] = None
+    sharesshort: Optional[int] = None
+    shortratio: Optional[float] = None
+    targetlowprice: Optional[float] = None
+    targethighprice: Optional[float] = None
+    targetmedianprice: Optional[float] = None
+    targetmeanprice: Optional[float] = None
+    
+class Weekly_Value(BaseModel):
+    '''Value subclass for weekly api call'''
     currentratio: Optional[float] = None
     debttoequity: Optional[float] = None
-    dividendrate: Optional[float] = None
-    dividendyield: Optional[float] = None
-    earningsgrowth: Optional[float] = None
-    earningsquarterlygrowth: Optional[float] = None
     enterprisetoebitda: Optional[float] = None
     enterprisetorevenue: Optional[float] = None
+    forwardpe: Optional[float] = None
+    trailingpe: Optional[float] = None
+    pricetosalestrailing12months: Optional[float] = None
+    pricetobook: Optional[float] = None
+    quickratio: Optional[float] = None
+    
+class Weekly_Dividend(BaseModel):
+    '''Dividend subclass for weekly api call'''
+    dividendrate: Optional[float] = None
+    dividendyield: Optional[float] = None
+    fiveyearavgdividendyield: Optional[float] = None
+    exdividenddate: Optional[int] = None
+    payoutratio: Optional[float] = None
+    
+class Weekly_Growth(BaseModel):
+    ''' Growth subclass for weekly api call'''
+    earningsgrowth: Optional[float] = None
+    earningsquarterlygrowth: Optional[float] = None
+    revenuegrowth: Optional[float] = None
+    revenuequarterlygrowth: Optional[str] = None
+    freecashflow: Optional[int] = None
+    
+class Weekly_Profitability(BaseModel):
+    ''' Profitability subclass for weekly api call'''
+    ebitdamargins: Optional[float] = None
+    grossmargins: Optional[float] = None
+    returnonassets: Optional[float] = None
+    returnonequity: Optional[float] = None
+    profitmargins: Optional[float] = None
+    
+class Weekly_Price(BaseModel):
+    '''Price subclass for weekly api call'''
+    currentprice: Optional[float] = None
     fiftydayaverage: Optional[float] = None
     fiftytwoweeklow: Optional[float] = None
     fiftytwoweekhigh: Optional[float] = None
-    currentprice: Optional[float] = None
-    fiveyearavgdividendyield: Optional[float] = None
-    earningsgrowth: Optional[float] = None
-    freecashflow: Optional[int] = None
-    marketcap: Optional[int] = None
-    pricetobook: Optional[float] = None
-    quickratio: Optional[float] = None
-    returnonassets: Optional[float] = None
-    returnonequity: Optional[float] = None
-    revenuequarterlygrowth: Optional[str] = None
-    sector: Optional[str] = None
-    industry: Optional[str] = None
+    twohundreddayaverage: Optional[float] = None
+    
+class Weekly_Momentum(BaseModel):
+    '''Momentum subclass for weekly api call'''
+    fiveyearaveragereturn: Optional[str] = None
+    threeyearaveragereturn: Optional[str] = None
+    
+class Weekly_Earnings(BaseModel):
+    '''Earnings subclass for weekly api call'''
+    forwardeps: Optional[float] = None
+    trailingeps: Optional[float] = None
+    
+class Weekly_Ownership(BaseModel):
+    '''Ownership subclass for weekly api call'''
+    heldpercentinsiders: Optional[float] = None
+    heldpercentinstitutions: Optional[float] = None
+    impliedsharesoutstanding: Optional[str] = None
+    floatshares: Optional[int] = None
+    sharesoutstanding: Optional[int] = None
+    
+class Weekly_Balance(BaseModel):
+    ''' Balance subclass for weekly api call'''
+    totalassets: Optional[str] = None
+    totalcash: Optional[int] = None
+    totalcashpershare: Optional[float] = None
+    totaldebt: Optional[int] = None
+    totalrevenue: Optional[int] = None
+    grossprofits: Optional[int] = None
+    operatingcashflow: Optional[int] = None
+    
+    
+
+#to have common attributes when reading or creating
+class WeeklyBase(BaseModel):
+    
+    id: Weekly_Id | None = None
+    
+    general: Union[Weekly_General, None] = None
+    
+    analysts: Union[Weekly_Analysts, None] = None
+    
+    value: Union[Weekly_Value, None] = None
+    
+    dividends: Union[Weekly_Dividend, None] = None
+    
+    growth: Union[Weekly_Growth, None] = None
+    
+    price: Union[Weekly_Price, None] = None
+    
+    momentum: Union[Weekly_Momentum, None] = None
+    
+    earnings: Union[Weekly_Earnings, None] = None
+    
+    ownership : Union[Weekly_Ownership, None] = None
+    
+    balance : Union[Weekly_Balance, None] = None  
+
     
 class WeeklyCreate(WeeklyBase):
     pass
